1d19701b9e20ba5494960ff58c1029a38dc00785
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-21 01:41:22 +0200

    search and replace right

--- src/parsers/LuaParser.zig
+++ src/parsers/LuaParser.zig
@@ -21,7 +21,7 @@ pub fn init(alloc: Allocator, in_path: []const u8, out_path: []const u8) !Self {
     assert(fs.path.isAbsolute(out_path));
 
     std.log.debug("Attempting to open dir {s}", .{in_path});
-    const in_dir = try fs.openDirAbsolute(in_path, .{});
+    const in_dir = try fs.openDirAbsolute(in_path, .{ .iterate = true });
 
     std.log.debug("Attempting to create {s}", .{out_path});
     try fs.makeDirAbsolute(out_path);
@@ -40,6 +40,57 @@ pub fn deinit(self: *Self) void {
     self.out_dir.close();
 }
 
+/// This function creates the lua config in the specificified location.
+///
+/// It extracts specific substitutions from the passed in plugins and then
+/// iterates over the input directory recursively. It copies non lua files
+/// directly and parses lua files for substitutions before copying the parsed
+/// files over.
+pub fn createConfig(self: Self, plugins: []const Plugin) !void {
+    const subs = try createSubsitutions(self.alloc, plugins);
+    defer {
+        for (subs) |sub| {
+            sub.deinit(self.alloc);
+        }
+        self.alloc.free(subs);
+    }
+
+    var walker = try self.in_dir.walk(self.alloc);
+    defer walker.deinit();
+
+    std.debug.print("Starting walk\n", .{});
+    while (try walker.next()) |entry| {
+        switch (entry.kind) {
+            .directory => {
+                try self.out_dir.makeDir(entry.path);
+            },
+            .file => {
+                if (std.mem.eql(u8, ".lua", std.fs.path.extension(entry.basename))) {
+                    std.debug.print("Lua file {s}\n", .{entry.basename});
+                    const in_file = try self.in_dir.openFile(entry.path, .{});
+                    const in_buf = try utils.mmapFile(in_file, .{});
+                    defer utils.unMmapFile(in_buf);
+
+                    const out_buf = try parseLuaFile(self.alloc, in_buf, subs);
+                    defer self.alloc.free(out_buf);
+
+                    const out_file = try self.out_dir.createFile(entry.path, .{});
+                    try out_file.writeAll(out_buf);
+                } else {
+                    std.debug.print("non-lua file {s}\n", .{entry.basename});
+                    try self.in_dir.copyFile(
+                        entry.path,
+                        self.out_dir,
+                        entry.path,
+                        .{},
+                    );
+                }
+            },
+            else => std.log.warn("Kind {}", .{entry.kind}),
+        }
+    }
+}
+
 fn createSubsitutions(alloc: Allocator, plugins: []const Plugin) ![]Substitution {
     var subs = std.ArrayList(Substitution).init(alloc);
 
@@ -47,28 +98,28 @@ fn createSubsitutions(alloc: Allocator, plugins: []const Plugin) ![]Substitution
         switch (plugin.tag) {
             .UrlNotFound => continue,
             .GitUrl => {
-                try subs.append(Substitution{
-                    .tag = .url,
-                    .from = plugin.url,
-                    .to = plugin.path,
-                });
+                try subs.append(try Substitution.init(
+                    alloc,
+                    plugin.url,
+                    plugin.path,
+                ));
             },
             .GithubUrl => {
-                try subs.append(Substitution{
-                    .tag = .url,
-                    .from = plugin.url,
-                    .to = plugin.path,
-                });
+                try subs.append(try Substitution.init(
+                    alloc,
+                    plugin.url,
+                    plugin.path,
+                ));
 
                 var url_splitter = std.mem.splitSequence(u8, plugin.url, "://github.com/");
                 _ = url_splitter.next().?;
                 const short_url = url_splitter.rest();
 
-                try subs.append(Substitution{
-                    .tag = .githubShort,
-                    .from = short_url,
-                    .to = plugin.path,
-                });
+                try subs.append(try Substitution.init(
+                    alloc,
+                    short_url,
+                    plugin.path,
+                ));
             },
         }
     }
@@ -76,3 +127,98 @@ fn createSubsitutions(alloc: Allocator, plugins: []const Plugin) ![]Substitution
     return try subs.toOwnedSlice();
 }
 
+// Good example of url usage:
+// https://sourcegraph.com/github.com/Amar1729/dotfiles/-/blob/dot_config/nvim/lua/plugins/treesitter.lua
+
+/// Returns a _new_ buffer, owned by the caller
+fn parseLuaFile(alloc: Allocator, input_buf: []const u8, subs: []const Substitution) ![]const u8 {
+    var iter = utils.BufIter{ .buf = input_buf };
+
+    var out_arr = std.ArrayList(u8).init(alloc);
+
+    while (!iter.isDone()) {
+        var chosen_sub: ?Substitution = null;
+        var chosen_skipped: ?[]const u8 = null;
+        inner: for (subs) |sub| {
+            std.debug.print("Looking for: {s}\n", .{sub.from});
+            const skip_str = iter.peekUntilBefore(sub.from) orelse continue :inner;
+            if (chosen_skipped == null or skip_str.len < chosen_skipped.?.len) {
+                chosen_skipped = skip_str;
+                chosen_sub = sub;
+            }
+        }
+
+        if (chosen_sub == null) {
+            // TODO: Move this out the loop pls
+            std.debug.print("Adding rest: ...\n", .{});
+            try out_arr.appendSlice(iter.rest() orelse "");
+        } else {
+            std.debug.print("Adding skipped: {?s}\n", .{chosen_skipped});
+            std.debug.print("Adding sub    : {s}\n", .{chosen_sub.?.to});
+            try out_arr.appendSlice(chosen_skipped.?);
+            try out_arr.appendSlice(chosen_sub.?.to);
+            iter.ptr += chosen_skipped.?.len;
+            iter.ptr += chosen_sub.?.from.len;
+        }
+    }
+
+    return out_arr.toOwnedSlice();
+}
+
+test "parseLuaFile copy" {
+    const alloc = std.testing.allocator;
+    const input_buf = "Hello world";
+    const expected = "Hello world";
+
+    const subs = &.{
+        Substitution{
+            .from = "asdf",
+            .to = "fdsa",
+        },
+    };
+
+    const out_buf = try parseLuaFile(alloc, input_buf, subs);
+    defer alloc.free(out_buf);
+
+    try std.testing.expectEqualStrings(expected, out_buf);
+}
+
+test "parseLuaFile simple sub" {
+    const alloc = std.testing.allocator;
+    const input_buf = "Hello world";
+    const expected = "hi world";
+
+    const subs = &.{
+        Substitution{
+            .from = "Hello",
+            .to = "hi",
+        },
+    };
+
+    const out_buf = try parseLuaFile(alloc, input_buf, subs);
+    defer alloc.free(out_buf);
+
+    try std.testing.expectEqualStrings(expected, out_buf);
+}
+
+test "parseLuaFile multiple subs" {
+    const alloc = std.testing.allocator;
+    const input_buf = "Hello world";
+    const expected = "world Hello";
+
+    const subs = &.{
+        Substitution{
+            .from = "Hello",
+            .to = "world",
+        },
+        Substitution{
+            .from = "world",
+            .to = "Hello",
+        },
+    };
+
+    const out_buf = try parseLuaFile(alloc, input_buf, subs);
+    defer alloc.free(out_buf);
+
+    try std.testing.expectEqualStrings(expected, out_buf);
+}
--- src/utils.zig
+++ src/utils.zig
@@ -37,11 +37,25 @@ pub const Plugin = struct {
 };
 
 pub const Substitution = struct {
-    tag: Tag,
     from: []const u8,
     to: []const u8,
 
     const Tag = enum { url, githubShort };
+
+    pub fn init(
+        alloc: Allocator,
+        from: []const u8,
+        to: []const u8,
+    ) !Substitution {
+        return Substitution{
+            .from = try std.fmt.allocPrint(alloc, "\"{s}\"", .{from}),
+            .to = try std.fmt.allocPrint(alloc, "dir = \"{s}\"", .{to}),
+        };
+    }
+    pub fn deinit(self: Substitution, alloc: Allocator) void {
+        alloc.free(self.to);
+        alloc.free(self.from);
+    }
 };
 
 const MmapConfig = struct {
@@ -78,7 +92,7 @@ pub fn unMmapFile(mapped_file: []align(mem.page_size) u8) void {
 }
 
 pub fn trim(input: []const u8) []const u8 {
-    return mem.trim(u8, input, " ;{}\"\n");
+    return mem.trim(u8, input, " \\;{}\"\n");
 }
 
 pub fn split(input: []const u8) mem.SplitIterator(u8, .sequence) {
@@ -87,4 +101,147 @@ pub fn split(input: []const u8) mem.SplitIterator(u8, .sequence) {
 
 pub fn eql(expected: []const u8, input: []const u8) bool {
     return mem.eql(u8, expected, trim(input));
+}
+
+pub const BufIter = struct {
+    buf: []const u8,
+    ptr: usize = 0,
+
+    pub fn skipUntil(self: *BufIter, until: []const u8) ?void {
+        if ((self.ptr + until.len) >= self.buf.len) return null;
+
+        for ((self.ptr)..self.buf.len) |ptr| {
+            std.debug.print("lookin at: '{s}'\n", .{self.buf[ptr..(ptr + until.len)]});
+            std.debug.print("lookin to: '{s}'\n", .{until});
+            if (!std.mem.eql(u8, self.buf[ptr..(ptr + until.len)], until))
+                continue;
+
+            std.debug.print("found    : '{s}'\n", .{until});
+            if (!std.mem.eql(u8, self.buf[ptr..(ptr + until.len)], until))
+                self.ptr = ptr;
+
+            return;
+        }
+        return null;
+    }
+
+    pub fn isDone(self: BufIter) bool {
+        return self.ptr >= self.buf.len;
+    }
+
+    pub fn thisChar(self: BufIter) ?u8 {
+        if (self.ptr >= self.buf.len) return null;
+        return self.buf[self.ptr];
+    }
+
+    pub fn thisWord(self: *BufIter) ?[]const u8 {
+        const str = self.peekThisWord() orelse return null;
+        defer self.ptr += str.len;
+        return str;
+    }
+
+    pub fn next(self: *BufIter) ?u8 {
+        defer self.ptr += 1;
+        return self.peek();
+    }
+
+    pub fn nextFor(self: *BufIter, n: usize) ?[]const u8 {
+        const str = self.peekFor(n) orelse return null;
+        defer self.ptr += str.len;
+        return str;
+    }
+
+    pub fn nextUntil(self: *BufIter, until: []const u8) ?[]const u8 {
+        const str = self.peekUntil(until) orelse return null;
+        defer self.ptr += str.len;
+        return str;
+    }
+
+    pub fn nextUntilAny(self: *BufIter, until: []const u8) ?[]const u8 {
+        const str = self.peekUntilAny(until) orelse return null;
+        defer self.ptr += str.len;
+        return str;
+    }
+
+    pub fn rest(self: *BufIter) ?[]const u8 {
+        const str = self.peekRest() orelse return null;
+        defer self.ptr += str.len;
+        return str;
+    }
+
+    pub fn peek(self: BufIter) ?u8 {
+        return self.peekBy(1);
+    }
+
+    pub fn peekBy(self: BufIter, n: usize) ?u8 {
+        if ((self.ptr + n) >= self.buf.len) return null;
+        return self.buf[self.ptr + n];
+    }
+
+    pub fn peekFor(self: BufIter, n: usize) ?[]const u8 {
+        if ((self.ptr + n) >= self.buf.len) return null;
+        return self.buf[self.ptr..(self.ptr + n)];
+    }
+
+    pub fn peekRest(self: BufIter) ?[]const u8 {
+        if (self.ptr >= self.buf.len) return null;
+        return self.buf[self.ptr..];
+    }
+
+    pub fn peekThisWord(self: BufIter) ?[]const u8 {
+        const char = self.thisChar() orelse return null;
+        if (isWhitespace(char)) return null;
+
+        for (self.ptr..self.buf.len) |ptr| {
+            if (!isWhitespace(self.buf[ptr])) continue;
+            return self.buf[self.ptr..ptr];
+        }
+        return self.buf[self.ptr..];
+    }
+
+    pub fn peekUntil(self: BufIter, until: []const u8) ?[]const u8 {
+        if ((self.ptr + until.len) >= self.buf.len) return null;
+
+        for ((self.ptr + 1)..self.buf.len) |ptr| {
+            if (!std.mem.eql(u8, self.buf[ptr..(ptr + until.len)], until))
+                continue;
+
+            return self.buf[self.ptr..(ptr + until.len)];
+        }
+        return null;
+    }
+
+    pub fn peekUntilBefore(self: BufIter, until: []const u8) ?[]const u8 {
+        if ((self.ptr + until.len) >= self.buf.len) return null;
+
+        for ((self.ptr)..(self.buf.len - until.len + 1)) |ptr| {
+            if (!std.mem.eql(u8, self.buf[ptr..(ptr + until.len)], until))
+                continue;
+
+            return self.buf[self.ptr..ptr];
+        }
+        return null;
+    }
+
+    pub fn peekUntilAny(self: BufIter, until: []const u8) ?[]const u8 {
+        if ((self.ptr + 1) >= self.buf.len) return null;
+
+        for ((self.ptr + 1)..self.buf.len) |ptr| {
+            const char = self.buf[ptr];
+            for (until) |until_char| {
+                if (char != until_char) continue;
+
+                return self.buf[self.ptr..ptr];
+            }
+        }
+        return null;
+    }
+};
+
+pub fn isWhitespace(char: u8) bool {
+    const whitespace = " \n";
+    inline for (whitespace) |ws_char| {
+        if (char == ws_char) return true;
+    }
+    return false;
 }
