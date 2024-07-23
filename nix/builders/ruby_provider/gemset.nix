de213ba5f777c872ab456ee5d7d5fc78faa19241
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-23 20:47:55 +0200

    Updated ruby provider
    Taken straight from
    https://github.com/Gerg-L/mnw/blob/4ea225024677e7c3a96080af8624fd3aa5dfa1b6/ruby_provider/Gemfile.lock

--- nix/builders/ruby_provider/gemset.nix
+++ nix/builders/ruby_provider/gemset.nix
@@ -1,34 +1,37 @@
-# Copied verbatim from https://github.com/NixOS/nixpkgs/blob/287ca00c3e9e0cc8112a38dde991966cebf896a8/pkgs/applications/editors/neovim/ruby_provider/gemset.nix
+# Copied verbatim from https://github.com/Gerg-L/mnw/blob/4ea225024677e7c3a96080af8624fd3aa5dfa1b6/ruby_provider/gemset.nix
 {
   msgpack = {
-    groups = ["default"];
-    platforms = [];
+    groups = [ "default" ];
+    platforms = [ ];
     source = {
-      remotes = ["https://rubygems.org"];
-      sha256 = "sha256-fPWiGi0w4OFlMZOIf3gd21jyeYhg5t/VdLz7kK9fD8Q=";
+      remotes = [ "https://rubygems.org" ];
+      sha256 = "1a5adcb7bwan09mqhj3wi9ib52hmdzmqg7q08pggn3adibyn5asr";
       type = "gem";
     };
-    version = "1.5.1";
+    version = "1.7.2";
   };
   multi_json = {
-    groups = ["default"];
-    platforms = [];
+    groups = [ "default" ];
+    platforms = [ ];
     source = {
-      remotes = ["https://rubygems.org"];
+      remotes = [ "https://rubygems.org" ];
       sha256 = "sha256-H9BBOLbkqQAX6NG4BMA5AxOZhm/z+6u3girqNnx4YV0=";
       type = "gem";
     };
     version = "1.15.0";
   };
   neovim = {
-    dependencies = ["msgpack" "multi_json"];
-    groups = ["default"];
-    platforms = [];
+    dependencies = [
+      "msgpack"
+      "multi_json"
+    ];
+    groups = [ "default" ];
+    platforms = [ ];
     source = {
-      remotes = ["https://rubygems.org"];
-      sha256 = "sha256-hRI43XGHGeqxMvpFjp0o79GGReiLXTkhwh5LYq6AQL4=";
+      remotes = [ "https://rubygems.org" ];
+      sha256 = "0gl34rriwwmj6p1s6ms0b311wmqaqiyc510svq31283jk0kp0qcd";
       type = "gem";
     };
-    version = "0.9.0";
+    version = "0.10.0";
   };
 }
