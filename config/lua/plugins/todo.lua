05f91d158ce871784a355ea7914acf39995ee2df
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-24 15:20:16 +0200

    Add IDEA

--- config/lua/plugins/todo.lua
+++ config/lua/plugins/todo.lua
@@ -12,7 +12,7 @@ return {
     keys = {
         { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
         { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-        { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME,TEST,TESTS<cr>",    desc = "Keywords I wanna track" },
+        { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME,TEST,TESTS,IDEA<cr>",    desc = "Keywords I wanna track" },
     },
     opts = {},
 }
