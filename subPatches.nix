eac6edecf43fa2cc8263db64ba9abcd9487a62cc
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-24 15:21:17 +0200

    Add idea

--- subPatches.nix
+++ subPatches.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
 let
+  # FIXME: Make sure every valid way of specifying a plugin is covered
+  # this might take a rewrite of the substitution system and then change the parsing. 
+  # IDEA: Give substitutions types. 
+  #   -> A type for github URLS which looks for 
+  #       url = ${String of {url}} 
+  #       ${String of {url}} 
+  #       url = ${String of {short_url}} 
+  #       ${String of {short_url}} 
+  #   -> A type for normal URLs
+  #       url = ${String of {url}} 
+  #       ${String of {url}} 
+  #   -> A type for string replacement
+  #       ${String of {given}} 
+  #   -> A type for general replacement
+  #       {given} 
+  # Here ${String of {x}} means `"x"` `'x'` or `[[x]]`
   githubURL = shortUrl: plugin:
     [
       {
