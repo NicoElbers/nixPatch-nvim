17a0737c2765c14e7f0219002d45e385ca1b84f3
Nico Elbers <nico.b.elbers@gmail.com>
2024-10-24 00:33:36 +0200

    neovim-nightly: depend on nixPatch nixpkgs
    Make the neovim nightly overlay depend on nixPatch's nixpkgs, in the
    further hope that linking issues will be harder to come across

--- flake.nix
+++ flake.nix
@@ -10,7 +10,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { nixpkgs, zls, ... }@inputs: 
