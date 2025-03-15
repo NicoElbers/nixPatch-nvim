a00cdb33da308f75c1281db759c33fbf98de169a
Nico Elbers <nico.b.elbers@gmail.com>
2025-03-15 22:14:48 +0100

    provider: Ensure $out/bin exists
    Closes #22

--- nix/builders/providerBuilder.nix
+++ nix/builders/providerBuilder.nix
@@ -19,18 +19,23 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ makeWrapper ];
 
   # Link all the providers into one directory
-  postBuild = 
-      ""
+  postBuild = ''
+      mkdir -p $out/bin
+      ''
       + lib.optionalString withPython3 ''
         makeWrapper ${python3Env.interpreter} $out/bin/${name}-python3 --unset PYTHONPATH ${builtins.concatStringsSep " " extraPython3WrapperArgs}
+
       ''
       + lib.optionalString withRuby ''
         ln -s ${rubyEnv}/bin/neovim-ruby-host $out/bin/${name}-ruby
+
       ''
       + lib.optionalString withNodeJs ''
         ln -s ${neovim-node-client}/bin/neovim-node-host $out/bin/${name}-node
+
       ''
       + lib.optionalString withPerl ''
         ln -s ${perlEnv}/bin/perl $out/bin/${name}-perl
+
       '';
 }
